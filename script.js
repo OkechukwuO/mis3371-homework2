7746b073b4d598bf9001fe10731dbb3ce6e72cbb
OkechukwuO <ocodocha@cougarnet.uh.edu>
2026-06-24 12:58:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.onload = showDate;
 
 
 //Review button function
-
+alert("script is connected");
 function reviewForm() {
     document.getElementById("reviewSection").innerHTML =
         "<h2>PLEASE REVIEW THIS INFORMATION</h2>";
