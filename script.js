548049b6419222763d45535190e0de51794f8ab2
OkechukwuO <ocodocha@cougarnet.uh.edu>
2026-06-26 18:20:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function reviewForm() {
     var middleInitial = document.getElementById("mi").value;
     var lastName = document.getElementById("lname").value;
     var dob = document.getElementById("dob").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
     
     document.getElementById("reviewSection").innerHTML =
         "<h2>PLEASE REVIEW THIS INFORMATION</h2>" +
@@ -25,5 +27,7 @@ function reviewForm() {
         lastName +
         "</p>" +
 
-        "<p>Date of Birth: " + dob + "</p>";
+        "<p>Date of Birth: " + dob + "</p>"
+        "<p>Email: " + email + "</p>" +
+        "<p>Phone Number: " + phone + "</p>";
 }
